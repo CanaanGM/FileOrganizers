a4c8049c9118a4238b84fbcc9d8e43fa56d50f45
CanaanGM <reakak1@gmail.com>
2022-03-05 22:06:48 +0200

    added more image types

--- commands/move_images.ps1
+++ commands/move_images.ps1
@@ -1,4 +1,4 @@
 $source=$args[0]
 $dist=$args[1]
 
-"$($source)\*.jpg", "$($source)\*.png" | Move-Item -Destination $dist
+"$($source)\*.jpg", "$($source)\*.png", "$($source)\*.gif", "$($source)\*.jpeg" | Move-Item -Destination $dist
