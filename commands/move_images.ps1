8cfbbafa565d6278f056f62861ad3ba26e744e7d
CanaanGM <reakak1@gmail.com>
2022-12-08 22:05:30 +0300

    made it move to a directory named 'random' instead of the root one

--- commands/move_images.ps1
+++ commands/move_images.ps1
@@ -1,4 +1,8 @@
 $source=$args[0]
 $dist=$args[1]
 
-"$($source)\*.jpg", "$($source)\*.png", "$($source)\*.jpeg" | Move-Item -Force -Destination $dist 
+if (!(Test-Path "$($dist)\random")){
+    New-Item "$($dist)\random" -ItemType Directory
+}
+
+"$($source)\*.jpg", "$($source)\*.png", "$($source)\*.jpeg" | Move-Item -Force -Destination "$($dist)\random"
